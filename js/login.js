7f932659a94b66b3f0892c0c6cbee74ac1e66eb2
Samina Chhatwal <csaminak@gmail.com>
2016-07-18 17:19:19 -0400

    updated form method to get and created token validation ajax request

--- js/login.js
+++ js/login.js
@@ -1,5 +1,28 @@
 (function(){
     'use strict';
 
-    
+    var $login = $('.login');
+    var token = $('input[name="token"]');
+
+    $('.login').on('submit', function(event){
+        event.preventDefault();
+        console.log(token.val());
+        authenticateToken(token.val());
+    });
+
+    function authenticateToken(token) {
+        return $.ajax({
+            url: 'https://api.github.com',
+            method: 'get',
+            headers: {'Authorization': 'token ' + token},
+            dataType: 'json'
+        })
+        .done(function(data){
+            console.log('token validated');
+        })
+        .fail(function(data){
+            console.log('token did not validate')
+        });
+    }
+
 })();
