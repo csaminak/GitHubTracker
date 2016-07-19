3af87eaef892cd871e6e87cd5293f95498e43360
Samina Chhatwal <csaminak@gmail.com>
2016-07-19 14:49:28 -0400

    moved ajax function from login into this view, since this is when it will be significant

--- js/repos.js
+++ js/repos.js
@@ -1,5 +1,23 @@
 (function(){
     'use strict';
 
+    /**
+     * Take the username/login from the user object and input into url to access
+     * all the repos the user has worked on, each element in the array is an object
+     * containing a single repo's information.
+     * @param  {String} username  retrieved from the user object to access repos.
+     * @return {Array}            all the repositories associated with the user.
+     */
+    function retrieveRepositories(username){
+        return $.ajax({
+            url: 'https://api.github.com/users/' + username + '/repos',
+            method: 'get',
+            headers: {'Authorization': 'token ' + $token},
+            dataType: 'json'
+        });
+    }
+    
+
+
 
 })();
