dd73ba632452d4007d4dafc6c920158ae4abc7ef
Samina Chhatwal <csaminak@gmail.com>
2016-07-19 15:43:36 -0400

    added img tag connected to user avatar url

--- js/myProfile.js
+++ js/myProfile.js
@@ -1,5 +1,19 @@
-(function(){
+(function(ns){
     'use strict';
+    window.ghProfile = ns = (ns || {});
+
+    var $myProfile = $('#myProfile');
+    var $loginView = $('section.login');
+    var $nav = $('.nav');
+
+    ns.displayMyProfile = function displayMyProfile(user) {
+        $myProfile.show();
+        $nav.show();
+        $loginView.hide();
+        $myProfile
+            .append('<img src=' + user.avatar_url + ' class="avatar" alt="userLogo" >');
+        console.log(user.avatar_url);
+    };
 
 
-})();
+})(window.ghProfile);
