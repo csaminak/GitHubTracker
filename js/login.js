2f677764ec2fc130a65f6ce7661588632cb09637
Samina Chhatwal <csaminak@gmail.com>
2016-07-18 20:57:16 -0400

    updated verification link to user instead of users to retrieve token user info

--- js/login.js
+++ js/login.js
@@ -1,24 +1,26 @@
 (function(){
     'use strict';
 
-    var $login = $('.login');
-    var token = $('input[name="token"]');
+    var $login = $('form.login');
+    var $token = $('input[name="token"]');
+    var user = {};
 
-    $('.login').on('submit', function(event){
+    $login.on('submit', function(event){
         event.preventDefault();
-        console.log(token.val());
-        authenticateToken(token.val());
+        console.log($token.val());
+        authenticateToken($token.val());
     });
 
     function authenticateToken(token) {
         return $.ajax({
-            url: 'https://api.github.com',
+            url: 'https://api.github.com/user',
             method: 'get',
             headers: {'Authorization': 'token ' + token},
             dataType: 'json'
         })
         .done(function(data){
-            console.log('token validated');
+            console.log('token validated')
+            console.log(data);
         })
         .fail(function(data){
             console.log('token did not validate')
