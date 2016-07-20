e701a637c20a847d04b2534b61a965b39a146b46
Samina Chhatwal <csaminak@gmail.com>
2016-07-20 15:09:21 -0400

    created sanity test function

--- test/specs/nav.js
+++ test/specs/nav.js
@@ -3,8 +3,18 @@
 
     var assert = chai.assert;
 
+    var fixtures = $('#fixtures').html();
+
     suite('Load View', function(){
 
+        test('make sure loadView function exists', function(){
+            assert.ok(window.spa.loadView, 'loadView exists b.c it can be called');
+            assert.strictEqual(typeof(window.spa.loadView), 'function', 'confirmed as a function');
+        });
+
+        // test('', function(){
+        //
+        // });
 
     });
 
