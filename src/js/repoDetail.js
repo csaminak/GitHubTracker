d9a76e89edac288745e725a6ac9a60f6e17937c2
Samina Chhatwal <csaminak@gmail.com>
2016-07-22 14:09:27 -0400

    created requestRepoData function with explanation of what it does and explained what displayRepoDetail will do

--- src/js/repoDetail.js
+++ src/js/repoDetail.js
@@ -4,14 +4,37 @@
 
     ns.repoDetail = {};
     ns.repoDetail.loadView = function initRepoDetail(viewHash) {
-        console.log(viewHash.split('/'));
-        // if this function is called, then this view is being shown
-        //needs to run some ajax call to find the specific data that needs to be shown.
+        var selectedRepo = viewHash.split('/');
+        var username = selectedRepo[1];
+        var repoName = selectedRepo[2];
+        requestRepoData(username, repoName)
+            .done(displayRepoDetail);
     };
 
-    ns.displaySelectedRepo = function displaySelectedRepo(repo) {
-        $('.nav')
-            .append('<li><a href=#repoDetail>Repo Detail</a></li>');
+    /**
+     * Uses the viewHash string passed in from loadView and splits it into an
+     * Array and uses the value at the index as the arguments used to send ajax
+     * get request to github to retrieve the individual repo object data.
+     * @param  {String}    username    used for url for the user who owns the repo
+     * @param  {String}    repoName    used for url for the specific repo to get
+     * @return {Object}    the repo object
+     */
+    function requestRepoData(username, repoName){
+        return $.ajax({
+            url: 'https://api.github.com/repos/' + username + '/' + repoName + '',
+            method: 'get',
+            headers: {'Authorization': 'token ' + window.ghTracker.$token},
+            dataType: 'json'
+        });
+    }
+
+    /**
+     * displays the view for the selected repo by using the data returned
+     * from requestRepoData.
+     * @param  {Object}     repo    repo data retrieved from the done promise.
+     * @return {void}
+     */
+    function displayRepoDetail(repo) {
         $('#repoDetail h2')
             .html(repo.name);
         $('#repoDetail p')
@@ -23,9 +46,9 @@
         $('.forks')
             .append(repo.forks);
         $('.createDate')
-            .append(repo.created_at);
+            .append(repo.created_at); //TODO change date format
         console.log(repo);
-    };
+    }
 
 
 
