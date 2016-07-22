fe34e94b57a3ee28a0591084f1fafe5d9bba030c
Samina Chhatwal <csaminak@gmail.com>
2016-07-22 12:51:42 -0400

    passed in viewHash as argument

--- src/js/repoDetail.js
+++ src/js/repoDetail.js
@@ -3,7 +3,8 @@
     window.ghTracker = ns = (ns || {});
 
     ns.repoDetail = {};
-    ns.repoDetail.loadView = function initRepoDetail() {
+    ns.repoDetail.loadView = function initRepoDetail(viewHash) {
+        console.log(viewHash.split('/'));
         // if this function is called, then this view is being shown
         //needs to run some ajax call to find the specific data that needs to be shown.
     };
