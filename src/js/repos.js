67d4ef08066b660d54c3bda40c837291b3982ac0
Samina Chhatwal <csaminak@gmail.com>
2016-07-22 11:21:03 -0400

    reverted back to previous state

--- src/js/repos.js
+++ src/js/repos.js
@@ -3,6 +3,7 @@
     window.ghTracker = ns = (ns || {});
 
 
+    var repoData = [];
     var $reposTable = $('#repos .table');
 
     ns.repos = {};
@@ -31,7 +32,7 @@
      * @return {void}
      */
     function displayRepos(repoData){
-        console.log();
+        console.log(repoData);
         repoData.forEach(function(repo){
             $reposTable //TODO Need to update where repo anchor will go to, load repoDetail
                 .append('<tr>\
@@ -57,6 +58,10 @@
             method: 'get',
             headers: {'Authorization': 'token ' + window.ghTracker.$token},
             dataType: 'json'
+        })
+        .done(function saveRepos(data){
+            repoData = data;
+            console.log(repoData);
         })
         .fail(function(xhr){ //TODO WHAT SHOULD FAIL DO????
             console.log(xhr);
