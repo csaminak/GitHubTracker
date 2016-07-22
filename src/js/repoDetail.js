166d1960202aab3db3ba89097d4236c9d0868b27
Samina Chhatwal <csaminak@gmail.com>
2016-07-22 11:52:47 -0400

    added a comment of what function should do

--- src/js/repoDetail.js
+++ src/js/repoDetail.js
@@ -5,14 +5,8 @@
     ns.repoDetail = {};
     ns.repoDetail.loadView = function initRepoDetail() {
         // if this function is called, then this view is being shown
-
+        //needs to run some ajax call to find the specific data that needs to be shown.
     };
-    //
-    //
-
-
-
-
 
 
 
