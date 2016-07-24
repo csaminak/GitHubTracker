ed56f977a8d165daa62585020b5741cb82e41206
Samina Chhatwal <csaminak@gmail.com>
2016-07-23 23:33:56 -0400

    added is function test

--- test/specs/myProfile.js
+++ test/specs/myProfile.js
@@ -2,10 +2,24 @@
     'use strict';
 
     var assert = window.chai.assert;
+    var tracker = window.ghTracker;
+    var fixtures = $('#fixtures').html();
 
-    suite('displayMyProfile', function(){
+    suite('displayMyProfile', function() {
 
-        
+        setup(function() {
+            $('#fixtures').html(fixtures);
+        });
+
+        teardown(function() {
+            $('#fixtures').html();
+        });
+
+        test('is displayMyProfile a function', function() {
+            assert.isFunction(tracker.displayMyProfile(),
+                                'displayMyProfile is a function');
+
+        });
 
 
 
