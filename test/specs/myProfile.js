09558747250f0281c025e5aaf4eafcd8f8156113
Samina Chhatwal <csaminak@gmail.com>
2016-07-24 21:45:43 -0400

    added a comment to test

--- test/specs/myProfile.js
+++ test/specs/myProfile.js
@@ -6,8 +6,7 @@
     var fixtures = $('#fixtures').html();
 
     suite('displayMyProfile', function() {
-        var user = {name: 'samina', created_at: '1234567890'};
-        var $name = $('.name');
+        var $name = $('.name'); //html is only able to be pulled from here?
 
         setup(function() {
             $('#fixtures').html(fixtures);
@@ -23,6 +22,7 @@
         });
 
         test('does displayMyProfile show the user\'s name', function() {
+            var user = {name: 'samina', created_at: '1234567890'};
             tracker.displayMyProfile(user);
             assert.deepEqual($name.text(), 'Name: ' + user.name,
                                     'user\'s name is displayed');
